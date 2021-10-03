5e251e6b3043f1bd0b2a89344a8df1b1eaf77c05
boburiy <bobur02k@gmail.com>
2021-10-03 14:54:45 +0500

    yangibobur

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const namber = 8; //data-type = namber
+const firstName = 'abdumajid'; // data-type = string
+const bool =false;//data-type = boolear
+
+const isMaried = true;
+
+//console.log(a);// data-type= null
+
+let lastName;
+console.log(firstName);
